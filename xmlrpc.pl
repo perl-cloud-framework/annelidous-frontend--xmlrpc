4f9283dde865aedde8eeb566a5c48ca971b39302
Eric Windisch <eric@grokthis.net>
2009-05-12 11:12:16 +0000

    added licensing info

--- xmlrpc.pl
+++ xmlrpc.pl
@@ -1,10 +1,27 @@
 #!/usr/bin/perl
+#
+# Annelidous - the flexibile cloud management framework
+# Copyright (C) 2009  Eric Windisch <eric@grokthis.net>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 my %CONFIG;
 
 # Note this function is GPL licensed
-# we'll take this out soon, though.
-# stolen, er.. borrowed from xen-shell
+# this should be allowed under terms of AGPLv3,
+# but we plan to take it out anyway.
 sub parseConfigurationFile
 {
     my ($file) = (@_);
